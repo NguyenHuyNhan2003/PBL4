249dbf947bef7d1b6e83dbd99e728f2ce0001f34
NguyenHuyNhan2003 <nguyenhuynhan.dn@gmail.com>
2023-11-11 07:40:54 +0700

    11/11/2023

--- PBL4_vscode/nodejs/db.js
+++ PBL4_vscode/nodejs/db.js
@@ -1,15 +1,40 @@
 const Client = require('mongodb').MongoClient;
 const Exceljs = require('exceljs');
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
 
-const excelPath = "../result/post_text_date.xlsx";
+const excelPath = "./result/post_text_date.xlsx";
 const mongoURI = 'mongodb+srv://nathan:PBL4@pbl4.xemvqhk.mongodb.net/PBL4?retryWrites=true&w=majority';
 
 //connect to mongoDB
-mongoose.connect(mongoURI)
-.then((result)=> console.log("Connected!"))
-.catch((err)=> console.log(err));
+Client.connect(mongoURI)
+.then( async(client)=> {
+        console.log('Connected!')
+        const db = client.db("PBL4");
+
+        const workbook = new Exceljs.Workbook();
+        await workbook.xlsx.readFile(excelPath);
+
+        const worksheet = workbook.getWorksheet(1);
+
+        const data = [];
+        worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
+        //the first row contains headers
+        if (rowNumber !== 1) {
+            const rowData = {};
+            row.eachCell((cell, colNumber) => {
+            //use column headers as keys
+            rowData[worksheet.getRow(1).getCell(colNumber).value] = cell.value;
+            });
+            data.push(rowData);
+        }
+        });
+        //insert data
+        await db.collection('Facebook_Posts').insertMany(data);
+
+        console.log('Inserted!');
+
+        //close connection
+        await client.close();
+        console.log('Connection closed!');
+}).catch((err)=> console.log(err));
 
 
